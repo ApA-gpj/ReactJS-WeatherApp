26e874026f2cb1e52061cfc1714d29ddc78ffd8c
ApA gPj <peymanjahangiri@gmail.com>
2024-01-14 22:50:25 +0330

    old api had typo

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = ({ updateSearchData }) => {
 
   const [searchValue, setSearchValue] = useState('');
   // const searchedData = `http://api.weatherstack.com/current?access_key=bee261edd16ebb9dbe08656ee3d602cc&query=${searchValue.toLowerCase()}`;
-  const searchedData = `https://https://api.openweathermap.org/data/2.5/weather?q=${searchValue.toLowerCase()}&appid=da936290743d81ba60a6d9b180bea3f9}`;
+  const searchedData = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue.toLowerCase()}&appid=da936290743d81ba60a6d9b180bea3f9}`;
 
   const submitHandler = async (e) => {
     e.preventDefault();
