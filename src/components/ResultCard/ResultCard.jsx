370dcbfe5e37a7c44de93cfc47cc70553e335780
ApA gPj <peymanjahangiri@gmail.com>
2024-01-15 01:15:55 +0330

    new api replaced

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -1,8 +1,9 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
+// /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 
 const ResultCard = ({ searchData }) => {
   const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const index = 0;
 
   useEffect(() => {
     if (searchData) {
@@ -12,48 +13,43 @@ const ResultCard = ({ searchData }) => {
     }
   }, [searchData]);
 
+  if (!isDataLoaded) {
+    return (
+      <div className="loader">Your Search Results Will Shown Here ...</div>
+    );
+  }
+
   return (
     <div className="result-card">
-      {!isDataLoaded ? (
-        <div className="loader">Your Search Results Will Shown Here ...</div>
-      ) : (
-        <>
-          {/* city infos */}
-          <div className="city-info">
-            <h2 className="city-name">
-              City Name: <span className="city-info">{searchData.location.name}</span>
-            </h2>
-            <p className="geography-info">
-              Country: <span className="country-info">{searchData.location.country}</span>
-            </p>
-            <p className="geography-info">
-              Region: <span className="region-info">{searchData.location.region}</span>
-            </p>
-            <p className="geography-info">
-              Local Time: <span className="time-info">{searchData.location.localtime}</span>
-            </p>
-          </div>
-          {/* weather infos */}
-          <div className="weather-info">
-            <h2 className="temperature">Temperature: {searchData.current.temperature}°C</h2>
-            <p className="weather-info">
-              Feels Like: {searchData.current.feelslike}°C<span className="feels-info"></span>
-            </p>
-            <p className="weather-info">
-              Humidity: {searchData.current.humidity}%<span className="humidity-info"></span>
-            </p>
-            <p className="weather-info">
-              UV-Index: {searchData.current.uv_index}<span className="uv-info"></span>
-            </p>
-            <p className="weather-info">
-              Wind Speed: {searchData.current.wind_speed} km/ph<span className="wind-info"></span>
-            </p>
-          </div>
-        </>
-
-      )}
+      {/* city infos */}
+      <div className="city-info">
+        <h2 className="city-name">
+          City Name: <span className="city-info">{searchData?.name}</span>
+        </h2>
+        <p className="geography-info">
+          Country:{" "}
+          <span className="country-info">{searchData?.sys?.country}</span>
+        </p>
+      </div>
+      {/* weather infos */}
+      <div className="weather-info">
+        <h2 className="temperature">
+          Temperature: {Math.floor(searchData?.main?.temp)}°C
+        </h2>
+        <p className="weather-info">
+          Feels Like: {Math.floor(searchData?.main?.feels_like)}°C
+          <span className="feels-info"></span>
+        </p>
+        <p className="weather-info">
+          Humidity: {searchData?.main?.humidity}%
+          <span className="humidity-info"></span>
+        </p>
+        <p className="weather-info">
+          Wind Speed: {Math.floor(searchData?.wind?.speed)} km/ph
+          <span className="wind-info"></span>
+        </p>
+      </div>
     </div>
-
   );
 };
 
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,41 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState } from "react";
 
 const SearchBar = ({ updateSearchData }) => {
-
-  const [searchValue, setSearchValue] = useState('');
-
-  const searchedData = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue.toLowerCase()}&appid=da936290743d81ba60a6d9b180bea3f9}`;
+  const [searchValue, setSearchValue] = useState("");
+  const apiKey = "da936290743d81ba60a6d9b180bea3f9";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue.toLowerCase()}&appid=${apiKey}&units=metric`;
 
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(searchedData)
-      const data = await response.json()
-      updateSearchData(data)
-      setSearchValue('')
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      updateSearchData(data);
+      setSearchValue("");
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-  }
+  };
 
   return (
-    <div >
-      <form onSubmit={submitHandler} className='search-container'>
-        <label className='search-label' htmlFor="searchinput">Enter City Name : </label>
-        <input className='search-input' type="search" name="searchinput" id="searchinput" autoFocus onChange={event => setSearchValue(event.target.value)} value={searchValue} />
-        <button className='search-btn' type="submit">Search</button>
+    <div>
+      <form onSubmit={submitHandler} className="search-container">
+        <label className="search-label" htmlFor="searchinput">
+          Enter City Name :{" "}
+        </label>
+        <input
+          className="search-input"
+          type="search"
+          name="searchinput"
+          id="searchinput"
+          autoFocus
+          onChange={(event) => setSearchValue(event.target.value)}
+          value={searchValue}
+        />
+        <button className="search-btn" type="submit">
+          Search
+        </button>
       </form>
     </div>
   );
